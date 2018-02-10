cc86cb981d8a29300d65781ff2ad391fe016ed58
VinzPich <pichot.vincent@gmail.com>
2018-02-10 13:42:00 +0100

    added Accordion for index

--- src/UE235/BibliothequeBundle/Controller/DefaultController.php
+++ src/UE235/BibliothequeBundle/Controller/DefaultController.php
@@ -4,19 +4,17 @@ namespace UE235\BibliothequeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-use UE235\BibliothequeBundle\Entity\Auteur;
-
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-    	$em = $this->container->get('doctrine')->getManager();
-		$categories = $em->getRepository('UE235BibliothequeBundle:Categorie')->findAll();
-    $livres = $em->getRepository('UE235BibliothequeBundle:Livre')->findAll();
+        $em = $this->container->get('doctrine')->getManager();
+        $categories = $em->getRepository('UE235BibliothequeBundle:Categorie')->findAll();
+        $livres = $em->getRepository('UE235BibliothequeBundle:Livre')->findAll();
 
-        return $this->render('UE235BibliothequeBundle:Default:index.html.twig', array(
-        	'categories' => $categories,
-          'livre' => $livres
-        ));
+        return $this->render('UE235BibliothequeBundle:Default:index.html.twig', [
+            'categories' => $categories,
+            'livres'     => $livres,
+        ]);
     }
 }
